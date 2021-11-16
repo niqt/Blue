650e6b4c1aa673dc36b618f3c1e7a57f57ac8072
Nicola De Filippo <nicola@nicoladefilippo.it>
2021-11-16 22:56:35 +0100

    Initial Commit

---
+++ Blue/BlueApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlueApp.swift
+//  Blue
+//
+//  Created by nicola on 16/11/21.
+//
+
+import SwiftUI
+
+@main
+struct BlueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
